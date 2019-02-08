f300800064285df2531760431b74740743c94df1
David van Keulen <davidvankeulen97@hotmail.com>
2019-02-08 15:34:08 +0100

    Zonder werkende library en tester

---
+++ NaryTreeDavid/test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using NaryTreeDavid;
+
+namespace NaryTreeDavid
+{
+    [TestFixture]
+    public class test
+    {
+        [TestCase]
+        public void testAddChildNode()
+        {
+            var root = AddChildNode(int 4,null);
+        }
+    }
+}
---
+++ NaryTreeDavidlib/NaryTreeDavidlib/TreeNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+namespace NaryTreeDavid
+{
+    public class TreeNode<T>
+    {
+        public T value;
+        public TreeNode<T> parent;
+        public List<TreeNode<T>> children;
+
+        public TreeNode(T ivalue,TreeNode<T> iparent)
+        {
+            value = ivalue;
+            parent = iparent;
+        }
+
+
+    }
+}
